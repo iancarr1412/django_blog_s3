b765019362de2d61bd1f12988a83f3b7334dfe64
iancarr1412 <iancarr1412@live.co.uk>
2018-09-17 20:51:27 +0100

    first commit

---
+++ posts/urls.py
@@ -0,0 +1,4 @@
+from django.conf import settings
+from django.conf.urls import include, url
+
+from posts import HomeView
---
+++ posts/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django. views. generic import TemplateView 
+
+class HomeView(Temp1ateView) : 
+"""Docstring for HomeView. """
+template _ name = 'home. html'
