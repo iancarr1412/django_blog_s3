4bbd8f0fa8b2e32ac44b356673c3bf0a0945a99a
iancarr1412 <iancarr1412@live.co.uk>
2018-09-19 08:53:06 +0100

    .gitignore is now working

--- assets/js/app.js
+++
@@ -1,4 +0,0 @@
-// $(document).ready(function(){
-//     $(h1).fadeIn({'duration': 3000});
-//     console.log('Hiya....');
-// });
--- posts/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django. views. generic import TemplateView 
-
-class HomeView(TemplateView) : 
-    """Docstring for HomeView. """
-
-    template_name = 'home.html'
